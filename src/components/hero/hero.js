75f29fe9d2bdb0681b9b2bd7db5f2c30d40d18d7
JD Fillmore <jd@dfill.me>
2019-02-02 15:19:27 -0500

    add section, h1 logo test text and paragraph text

---
+++ src/components/hero/hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+  render() {
+    return (
+      <div>
+        <section className="hero">     
+          <h1>(Logo)</h1>
+          <p id="subtitle">Welcome to the Final Fantasy 7 Dex.</p>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Hero;
