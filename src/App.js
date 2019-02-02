60e7b184753409b03b1d2743d3c6742ded3ecb72
JD Fillmore <jd@dfill.me>
2019-02-02 14:26:57 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      {/* Components go here */}
+      </div>
+    );
+  }
+}
+
+export default App;
