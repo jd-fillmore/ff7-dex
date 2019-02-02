835c4736f034aa2fdb9442dfc0e323fe59011aad
JD Fillmore <jd@dfill.me>
2019-02-02 15:21:04 -0500

    add props text to hero

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Hero></Hero>
+        <Hero text="Welcome to the Final Fantasy 7 Dex."></Hero>
         <Footer text='Powered by Reactjs, Flexbox and SCSS.'></Footer>
       </div>
     );
