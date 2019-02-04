f19acb4a76662649b226d6316a188765076d0904
jdf-tw <jdfillmore@thrillworks.com>
2019-02-04 09:40:37 -0500

    import components and styles, section, container and cols

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import Hero from './components/hero/hero';
-import Footer from './components/footer/footer';
+
+// Components
+import Content from './components/content/content';
+import Sidebar from './components/sidebar/sidebar';
+
+// Styles
+import "./components/card-overview/card-overview.scss";
+import "./components/base/base.scss";
+import "./components/sidebar/sidebar.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Hero text="Welcome to the Final Fantasy 7 Dex."></Hero>
-        <Footer text='Powered by Reactjs, Flexbox and SCSS.'></Footer>
+        <section className="main">
+          <div className="container">
+            <div className="col">
+              <Sidebar></Sidebar>
+            </div>
+            <div className="col">
+              <Content></Content>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
