8454ab9adda65c9d0c583c6efd1b87583ee584ef
Fillmore <jfillmore@ehs.com>
2019-08-15 16:21:03 -0400

    adding big macs

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 
 class Intro extends Component {
-  
   render() {
     return (
       <div>
         <section className="intro">
           <img src={this.props.logo} alt="FF7 Meteor" />
-          <h1>{this.props.title}</h1>
+          <h1>Big Macs</h1>
           <p>{this.props.text}</p>
         </section>
       </div>
